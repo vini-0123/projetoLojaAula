3096c54a17e76a2fbb0a88a50d7833374a84d2de
Vinicius de Oliveira <sonicdahora@gmail.com>
2025-04-04 09:04:14 -0300

    Update 1

--- vinigames/factory/firebase.js
+++ vinigames/factory/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLzjs-cFPwJLeyvG-nuRkSWe6l7mAh-Iw",
-  authDomain: "bd-vinigames.firebaseapp.com",
-  projectId: "bd-vinigames",
-  storageBucket: "bd-vinigames.firebasestorage.app",
-  messagingSenderId: "723774311494",
-  appId: "1:723774311494:web:ffa942972af4023fb0ce47"
+  -
 };
 Firebase.initializeApp(firebaseConfig);
 const conexao = Firebase.firestore();
